8d69b250ee9d9e23fa7b3d4f6afab668729c154c
eglantine-vialaneix <eglantine.vialaneix@epfl.ch>
2026-03-12 11:00:51 +0100

    added first version of the data labelling activity (unresponsive at that point)

---
+++ app/data_labelling/page.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+const imgWhiteBox1 = "http://localhost:3845/assets/49b2365017b48dc1b067894b1f06f95b6454194e.png";
+const imgUtahraptor = "http://localhost:3845/assets/d3f74ab729767fe5addef86660564a2dddb0a401.png";
+const imgTyrannosaurus = "http://localhost:3845/assets/dcdab5505195382f205aa7253e6b354620605ebd.png";
+const imgStyracosaurus = "http://localhost:3845/assets/556c568d0b01d9a4e13856a03207e3f0bfbfef6e.png";
+const imgStegosaurus = "http://localhost:3845/assets/3be43893a4273359fc07e3097d97954768450f9d.png";
+const imgSpinosaurus = "http://localhost:3845/assets/b853b31aa1ded0d60f861a8ff8c4cd690cbab560.png";
+const imgPlateosaurus = "http://localhost:3845/assets/2b9cdb931699927099810ca5d4f35e19938ece32.png";
+const imgMegalosaurus = "http://localhost:3845/assets/7c7a60cfdc4ed952c85dc5655e7211c976f432d8.png";
+const imgGallimimus = "http://localhost:3845/assets/a15fbeb53379158463a4e3b3b26d9f4f8f570b6b.png";
+const imgBrachiosaurus = "http://localhost:3845/assets/44d65eae8de44cf0f8a3d8c9fcd53657c2ef42b6.png";
+const imgApatosaurus = "http://localhost:3845/assets/77944b610335698810615cfa04c90ebecc17b180.png";
+const imgLine = "http://localhost:3845/assets/fdd2aaf1e67a4f131bc570d32f7c22e196f531e7.svg";
+const imgLine1 = "http://localhost:3845/assets/e58e88400af058a73c479c1354a09e6d8cd537c6.svg";
+
+export default function WhiteBox() {
+  return (
+    <div className="content-stretch flex items-center justify-center py-[60px] relative size-full" data-name="WhiteBox - 1" data-node-id="4:13">
+      <div aria-hidden="true" className="absolute inset-0 pointer-events-none">
+        <img alt="" className="absolute max-w-none object-cover size-full" src={imgWhiteBox1} />
+        <div className="absolute inset-0" style={{ backgroundImage: "url('data:image/svg+xml;utf8,<svg viewBox=\\'0 0 1440 1024\\' xmlns=\\'http://www.w3.org/2000/svg\\' preserveAspectRatio=\\'none\\'><rect x=\\'0\\' y=\\'0\\' height=\\'100%\\' width=\\'100%\\' fill=\\'url(%23grad)\\' opacity=\\'0.25\\'/><defs><radialGradient id=\\'grad\\' gradientUnits=\\'userSpaceOnUse\\' cx=\\'0\\' cy=\\'0\\' r=\\'10\\' gradientTransform=\\'matrix(4.4087e-15 51.2 -72 3.1351e-15 720 512)\\'><stop stop-color=\\'rgba(102,102,102,1)\\' offset=\\'0\\'/><stop stop-color=\\'rgba(77,77,77,1)\\' offset=\\'0.25\\'/><stop stop-color=\\'rgba(51,51,51,1)\\' offset=\\'0.5\\'/><stop stop-color=\\'rgba(26,26,26,1)\\' offset=\\'0.75\\'/><stop stop-color=\\'rgba(13,13,13,1)\\' offset=\\'0.875\\'/><stop stop-color=\\'rgba(6,6,6,1)\\' offset=\\'0.9375\\'/><stop stop-color=\\'rgba(0,0,0,1)\\' offset=\\'1\\'/></radialGradient></defs></svg>')" }} />
+      </div>
+      <div className="content-stretch flex flex-[1_0_0] gap-[50px] h-full items-center justify-end min-h-px min-w-px px-[50px] relative" data-node-id="10:189">
+        <div className="content-stretch flex items-center py-[191px] relative shrink-0" data-name="left" data-node-id="11:6">
+          <div className="blur-[4.5px] content-center flex flex-wrap gap-y-[49px] items-center justify-center pr-[137px] relative shrink-0" data-name="all cards left" data-node-id="10:175">
+            <div className="h-[255px] mr-[-137px] relative shrink-0 w-[180px]" data-name="utah" data-node-id="19:6654">
+              <div className="absolute h-[255px] left-0 rounded-[15px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] top-0 w-[180px]" data-name="Utahraptor" data-node-id="19:6655">
+                <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-[15px] size-full" src={imgUtahraptor} />
+              </div>
+            </div>
+            <div className="h-[256px] mr-[-137px] relative shrink-0 w-[180px]" data-name="tyra" data-node-id="19:6656">
+              <div className="absolute h-[256px] left-0 rounded-[15px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] top-0 w-[180px]" data-name="Tyrannosaurus" data-node-id="19:6657">
+                <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-[15px] size-full" src={imgTyrannosaurus} />
+              </div>
+            </div>
+            <div className="h-[255px] mr-[-137px] relative shrink-0 w-[180px]" data-name="styra" data-node-id="19:6658">
+              <div className="absolute h-[255px] left-0 rounded-[15px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] top-0 w-[180px]" data-name="Styracosaurus" data-node-id="19:6659">
+                <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-[15px] size-full" src={imgStyracosaurus} />
+              </div>
+            </div>
+            <div className="h-[255px] mr-[-137px] relative shrink-0 w-[180px]" data-name="stego" data-node-id="19:6660">
+              <div className="absolute h-[255px] left-0 rounded-[15px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] top-0 w-[180px]" data-name="Stegosaurus" data-node-id="19:6661">
+                <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-[15px] size-full" src={imgStegosaurus} />
+              </div>
+            </div>
+            <div className="h-[255px] mr-[-137px] relative shrink-0 w-[180px]" data-name="spino" data-node-id="19:6662">
+              <div className="absolute h-[255px] left-0 rounded-[15px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] top-0 w-[180px]" data-name="Spinosaurus" data-node-id="19:6663">
+                <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-[15px] size-full" src={imgSpinosaurus} />
+              </div>
+            </div>
+            <div className="h-[255px] mr-[-137px] relative shrink-0 w-[180px]" data-name="plateo" data-node-id="19:6664">
+              <div className="absolute h-[255px] left-0 rounded-[15px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] top-0 w-[180px]" data-name="Plateosaurus" data-node-id="19:6665">
+                <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-[15px] size-full" src={imgPlateosaurus} />
+              </div>
+            </div>
+            <div className="h-[255px] mr-[-137px] relative shrink-0 w-[180px]" data-name="megalo" data-node-id="19:6666">
+              <div className="absolute h-[255px] left-0 rounded-[15px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] top-0 w-[180px]" data-name="Megalosaurus" data-node-id="19:6667">
+                <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-[15px] size-full" src={imgMegalosaurus} />
+              </div>
+            </div>
+            <div className="h-[257px] mr-[-137px] relative shrink-0 w-[181px]" data-name="galli" data-node-id="19:6668">
+              <div className="absolute h-[257px] left-0 rounded-[15px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] top-0 w-[181px]" data-name="Gallimimus" data-node-id="19:6669">
+                <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-[15px] size-full" src={imgGallimimus} />
+              </div>
+            </div>
+            <div className="h-[256px] mr-[-137px] relative shrink-0 w-[180px]" data-name="brachio" data-node-id="19:6670">
+              <div className="absolute h-[256px] left-0 rounded-[15px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] top-0 w-[180px]" data-name="Brachiosaurus" data-node-id="19:6671">
+                <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-[15px] size-full" src={imgBrachiosaurus} />
+              </div>
+            </div>
+            <div className="h-[256px] mr-[-137px] relative shrink-0 w-[180px]" data-name="nb of cards left" data-node-id="11:10">
+              <div className="absolute h-[256px] left-0 rounded-[15px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] top-0 w-[180px]" data-node-id="11:7" style={{ backgroundImage: "url('data:image/svg+xml;utf8,<svg viewBox=\\'0 0 180 256\\' xmlns=\\'http://www.w3.org/2000/svg\\' preserveAspectRatio=\\'none\\'><rect x=\\'0\\' y=\\'0\\' height=\\'100%\\' width=\\'100%\\' fill=\\'url(%23grad)\\' opacity=\\'1\\'/><defs><radialGradient id=\\'grad\\' gradientUnits=\\'userSpaceOnUse\\' cx=\\'0\\' cy=\\'0\\' r=\\'10\\' gradientTransform=\\'matrix(5.5109e-16 12.8 -9 7.8377e-16 90 128)\\'><stop stop-color=\\'rgba(249,247,244,1)\\' offset=\\'0\\'/><stop stop-color=\\'rgba(230,223,216,1)\\' offset=\\'1\\'/></radialGradient></defs></svg>')" }} />
+              <div className="-translate-x-1/2 -translate-y-1/2 absolute border-2 border-black border-solid content-stretch flex items-center justify-center left-1/2 pl-[35px] pr-[34px] py-[18px] rounded-[50px] size-[100px] top-1/2" data-node-id="11:11">
+                <div className="flex flex-col font-[family-name:var(--font,'Inter:Medium',sans-serif)] font-[var(--font-medium,normal)] font-medium justify-center leading-[0] relative shrink-0 text-[48px] text-black text-center whitespace-nowrap" data-node-id="11:8">
+                  <p className="leading-[1.34]">9</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="content-stretch flex h-[892px] items-center justify-center relative shrink-0 w-[629px]" data-name="Main card" data-node-id="10:187">
+          <div className="content-stretch flex h-full items-center relative shrink-0" data-name="apato" data-node-id="19:6753">
+            <div className="h-[892px] relative rounded-[20px] shadow-[-7px_7px_4px_0px_rgba(0,0,0,0.25)] shrink-0 w-[628px]" data-name="Apatosaurus" data-node-id="19:12698">
+              <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none rounded-[20px] size-full" src={imgApatosaurus} />
+            </div>
+          </div>
+        </div>
+        <div className="content-stretch flex flex-col gap-[36px] h-full items-center justify-center relative shrink-0 w-[333px]" data-name="right" data-node-id="15:5918">
+          <div className="bg-[var(--background\/background,#f5f5f5)] border border-[var(--border,#dedee0)] border-solid flex-[1_0_0] min-h-px min-w-px relative rounded-[var(--dimensions\/radius\/rounded-3xl,24px)] w-full" data-name="all herbivores" data-node-id="19:12746">
+            <div className="absolute inset-0 pointer-events-none rounded-[inherit] shadow-[inset_-7px_7px_4px_0px_rgba(0,0,0,0.25)]" />
+          </div>
+          <div className="backdrop-blur-[var(--blur,0px)] bg-[var(--success\/success,#17c964)] content-stretch flex items-center justify-center px-[10px] py-[8px] relative rounded-[22px] shrink-0 w-full" data-name="Chip" data-node-id="15:5919">
+            <p className="font-[family-name:var(--font,'Inter:Medium',sans-serif)] font-[var(--font-medium,normal)] font-medium leading-[1.43] relative shrink-0 text-[24px] text-[color:var(--success\/success-foreground,#18181b)] whitespace-nowrap" data-node-id="15:5921">
+              Herbivore
+            </p>
+          </div>
+          <div className="content-stretch flex h-[12px] items-center justify-center max-h-[12px] relative rounded-[var(--dimensions\/radius\/rounded-sm,4px)] shrink-0 w-full" data-name="Separator" data-node-id="10:157">
+            <div className="flex-[1_0_0] h-0 min-h-px min-w-px relative" data-name="Line" data-node-id="I10:157;10:96">
+              <div className="absolute inset-[-0.5px_-0.35%]">
+                <img alt="" className="block max-w-none size-full" src={imgLine} />
+              </div>
+            </div>
+            <div className="content-stretch flex items-center justify-center px-[12px] relative shrink-0" data-name="container" data-node-id="I10:157;10:97">
+              <p className="font-[family-name:var(--font,'Inter:Medium',sans-serif)] font-[var(--font-medium,normal)] font-medium leading-[1.34] relative shrink-0 text-[16px] text-[color:var(--foreground\/muted,#71717a)] whitespace-nowrap" data-node-id="I10:157;10:98">
+                OR
+              </p>
+            </div>
+            <div className="flex-[1_0_0] h-0 min-h-px min-w-px relative" data-name="Line" data-node-id="I10:157;10:99">
+              <div className="absolute inset-[-0.5px_-0.35%]">
+                <img alt="" className="block max-w-none size-full" src={imgLine1} />
+              </div>
+            </div>
+          </div>
+          <div className="backdrop-blur-[var(--blur,0px)] bg-[var(--danger\/danger,#ff383c)] content-stretch flex items-center justify-center px-[10px] py-[8px] relative rounded-[22px] shrink-0 w-full" data-name="Chip" data-node-id="15:5929">
+            <p className="font-[family-name:var(--font,'Inter:Medium',sans-serif)] font-[var(--font-medium,normal)] font-medium leading-[1.43] relative shrink-0 text-[24px] text-[color:var(--danger\/danger-foreground,#fcfcfc)] whitespace-nowrap" data-node-id="15:5931">
+              Carnivore
+            </p>
+          </div>
+          <div className="bg-[var(--background\/background,#f5f5f5)] border border-[var(--border,#dedee0)] border-solid flex-[1_0_0] min-h-px min-w-px relative rounded-[var(--dimensions\/radius\/rounded-3xl,24px)] w-full" data-name="all carnivores" data-node-id="19:12750">
+            <div className="absolute inset-0 pointer-events-none rounded-[inherit] shadow-[inset_-7px_7px_4px_0px_rgba(0,0,0,0.25)]" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
