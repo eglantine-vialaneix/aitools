978f95ed45af5c20142760178c287b730fac5ee9
eglantine-vialaneix <eglantine.vialaneix@epfl.ch>
2026-05-01 16:47:21 +0200

    BACKEND: storing which dinos are labelled by students in constant file

---
+++ app/backend/constants.py
@@ -0,0 +1,12 @@
+LABELLED_BY_STUDENTS_DINOS = {
+                "Apatosaurus": "herbivore",
+                "Brachiosaurus": "herbivore",
+                "Gallimimus": "carnivore",
+                "Megalosaurus": "carnivore",
+                "Plateosaurus": "herbivore",
+                "Spinosaurus": "carnivore",
+                "Stegosaurus": "herbivore",
+                "Styracosaurus": "herbivore",
+                "Tyrannosaurus": "carnivore",
+                "Utahraptor": "carnivore",
+                }           
